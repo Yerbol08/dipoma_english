57a7c7c10133c77b33215a17741d2db4e15481e1
Your Name <you@example.com>
2021-04-16 18:40:50 +0600

    part 3

--- app/src/main/java/kz/tengrilab/diploma_english/MainActivity.java
+++ app/src/main/java/kz/tengrilab/diploma_english/MainActivity.java
@@ -1,13 +1,17 @@
 package kz.tengrilab.diploma_english;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.firebase.ui.auth.AuthUI;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.badge.BadgeUtils;
 import com.google.firebase.auth.FirebaseAuth;
 
@@ -50,6 +54,25 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 startActivity(new Intent(getApplicationContext(), Lesson3Activity.class));
+            }
+        });
+
+        Button btn4 = (Button) findViewById(R.id.btn4);
+        btn4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AuthUI.getInstance().signOut(getApplicationContext())
+                        .addOnCompleteListener(new OnCompleteListener<Void>() {
+                            @Override
+                            public void onComplete(@NonNull Task<Void> task) {
+
+                                Toast.makeText(getApplicationContext(), "Выход выполнен", Toast.LENGTH_SHORT).show();
+
+                                moveTaskToBack(true);
+                                finish();
+
+                            }
+                        });
             }
         });
 
